b803b9987219c2028d1aa02757c1ea4605fc01ef
jhariel8 <jhariel8@gmail.com>
2022-03-31 09:52:29 -0500

    initial commit

---
+++ src/test/java/com/alphasegroup/attendancetracker/AttendancetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphasegroup.attendancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
