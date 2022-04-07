02c63392688bdb00888dbc1f1baa2f6f0020b24b
jhariel8 <jhariel8@gmail.com>
2022-04-07 12:30:18 -0500

    installing thymeleaf

--- src/main/java/com/alphasegroup/attendancetracker/AttendancetrackerApplication.java
+++ src/main/java/com/alphasegroup/attendancetracker/AttendancetrackerApplication.java
@@ -3,6 +3,7 @@ package com.alphasegroup.attendancetracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
-@EnableWebMvc
+//@EnableWebMvc
 public class AttendancetrackerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/alphasegroup/attendancetracker/Controllers/ClassMeetingController.java
+++ src/main/java/com/alphasegroup/attendancetracker/Controllers/ClassMeetingController.java
@@ -1,12 +1,13 @@
 package com.alphasegroup.attendancetracker.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ClassMeetingController {
 
-	@GetMapping("/classmeeting")
+	@RequestMapping("/classmeeting")
 	public String generateClassMeetingPage() {
 		return "generateclassmeeting";
 	}
