7d37f6ec0c94f260a302cad71981a6d0f13da40e
jhariel8 <jhariel8@gmail.com>
2022-04-08 18:06:56 -0500

    generate class meeting form

--- src/main/java/com/alphasegroup/attendancetracker/Controllers/ClassMeetingController.java
+++ src/main/java/com/alphasegroup/attendancetracker/Controllers/ClassMeetingController.java
@@ -2,13 +2,19 @@ package com.alphasegroup.attendancetracker.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ClassMeetingController {
 
 	@RequestMapping("/classmeeting")
 	public String generateClassMeetingPage() {
+		return "generateclassmeeting";
+	}
+
+	@RequestMapping("/generatemeeting")
+	public String generateClassMeeting(@RequestParam(name="classid", required=true) String classid, 
+		@RequestParam(name="duration", required = true) int duration) {
 		return "generateclassmeeting";
 	}
 
---
+++ src/main/resources/static/js/generateclassmeeting.js
@@ -0,0 +1,18 @@
+// Makes a request to generate a class meeting
+function generateClassMeeting() {
+    var classid = document.forms["genClassForm"]["classid"];
+    var duration = document.forms["genClassForm"]["duration"];
+
+    console.log(duration.value);
+
+    if(classid.value == "") {
+        var error = document.getElementById("formerror");
+        error.innerHTML = "A class ID must be provided.";
+        return false;
+    }
+    if(duration.value == "" || duration.value < 1) {
+        var error = document.getElementById("formerror");
+        error.innerHTML = "You must give a duration that is longer than 0 hours.";
+        return false; 
+    }
+}
