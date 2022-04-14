4705ef78f105ebda01fce0e2473f73c42f5957b7
jhariel8 <jhariel8@gmail.com>
2022-04-14 14:24:44 -0500

    qr code page

--- src/main/java/com/alphasegroup/attendancetracker/Controllers/ClassMeetingController.java
+++ src/main/java/com/alphasegroup/attendancetracker/Controllers/ClassMeetingController.java
@@ -3,15 +3,21 @@ package com.alphasegroup.attendancetracker.Controllers;
 import java.sql.Timestamp;
 import java.time.Instant;
 
+import com.alphasegroup.attendancetracker.DataAccess.ClassMeetingRepository;
 import com.alphasegroup.attendancetracker.Models.ClassMeeting;
 import com.alphasegroup.attendancetracker.Models.Section;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ClassMeetingController {
+
+	@Autowired
+	private ClassMeetingRepository classMeetingRepository;
 
 	@RequestMapping("/classmeeting")
 	public String generateClassMeetingPage() {
@@ -20,17 +26,41 @@ public class ClassMeetingController {
 
 	@RequestMapping("/generatemeeting")
 	public String generateClassMeeting(@RequestParam(name="sectionid", required=true) String sectionid, 
-		@RequestParam(name="duration", required = true) int duration) {
+		@RequestParam(name="duration", required = true) int duration, Model model) {
 		
 		ClassMeeting cm = new ClassMeeting();
-		cm.setTimestamp(new Timestamp(Instant.now().getEpochSecond()));
+		cm.setTimestamp(new Timestamp(Instant.now().toEpochMilli()));
 		cm.setDuration(duration);
 
 		Section s = new Section();
 		s.setId(Integer.parseInt(sectionid));
 		cm.setSectionId(s);
 
-		return "generateclassmeeting";
+		ClassMeeting created = null;
+
+		try {
+			created = classMeetingRepository.save(cm);
+		}
+		catch(Exception ex) {
+			model.addAttribute("error", "There was a problem creating the class meeting. Try again.");
+			return "generateclassmeeting";
+		}
+
+		String uri = "https://api.qrserver.com/v1/create-qr-code/?" +
+					"size=200x200&data=www.google.com";
+		// URI fin = null;
+		// try {
+		// 	fin = new URI(uri);
+		// }
+		// catch(Exception e) {}
+
+		// HttpClient http = HttpClient.newHttpClient();
+		// HttpRequest httpReq = HttpRequest.newBuilder().GET().uri(fin).build();
+
+		model.addAttribute("qrcode", uri);
+		model.addAttribute("classmeeting", created);
+
+		return "qrcodepage";
 	}
 
 }
